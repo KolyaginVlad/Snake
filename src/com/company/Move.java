7dceef828c729e3050747d4f2a03e7d5948fd363
SofiaMarchinskaya <52950694+SofiaMarchinskaya@users.noreply.github.com>
2019-07-16 14:58:31 +0300

    Update Move.java

--- src/com/company/Move.java
+++ src/com/company/Move.java
@@ -1,11 +1,25 @@
 package com.company;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
-public class Move implements ActionListener {
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        
-    }
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class Move extends KeyAdapter {
+public void keyPressed(KeyEvent event){
+if(event.getKeyCode()==KeyEvent.VK_DOWN){
+
 }
+if(event.getKeyCode()==KeyEvent.VK_UP){
+
+}
+
+if(event.getKeyCode()==KeyEvent.VK_LEFT){
+
+}
+if(event.getKeyCode()==KeyEvent.VK_RIGHT){
+
+}
+}
+    }
+
