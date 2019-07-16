a894ba419cb719301758e950b24d420fd0cf6cf9
pro100fantom <50174448+pro100fantom@users.noreply.github.com>
2019-07-16 15:56:38 +0300

    update

--- src/com/company/Move.java
+++ src/com/company/Move.java
@@ -6,28 +6,28 @@ import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
 public class Move extends KeyAdapter{
-    static int step = 30;
+
 
 public void keyPressed(KeyEvent event){
 if(event.getKeyCode()==KeyEvent.VK_DOWN&&HeadSneake.direction!=HeadSneake.UP){
 HeadSneake.direction=HeadSneake.DOWN;
-HeadSneake.y+=step;
+
 }
 if(event.getKeyCode()==KeyEvent.VK_UP&&HeadSneake.direction!=HeadSneake.DOWN){
 HeadSneake.direction = HeadSneake.UP;
-    HeadSneake.y-=step;
+
 }
 
 if(event.getKeyCode()==KeyEvent.VK_LEFT&&HeadSneake.direction!=HeadSneake.RRIGHT){
 HeadSneake.direction = HeadSneake.LEFT;
-    HeadSneake.x-=step;
+
 }
 if(event.getKeyCode()==KeyEvent.VK_RIGHT&&HeadSneake.direction!=HeadSneake.LEFT){
 HeadSneake.direction = HeadSneake.RRIGHT;
-    HeadSneake.x+=step;
+
 }
 if(event.getKeyCode()==KeyEvent.VK_SPACE){
-
+    Window.game=true;
 }
 }
 
