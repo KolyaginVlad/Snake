b401cae9ca1b726ffe80641a11f7743b80320504
pro100fantom <50174448+pro100fantom@users.noreply.github.com>
2019-07-16 11:13:29 +0300

    r

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,13 +5,18 @@ import javax.swing.*;
 public class Main {
 
     public static void main(String[] args) {
-	new Window();
+        new Window();
     }
 
 
 }
+
 class Window extends JFrame{
     public Window(){
-
+    setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    setSize(600,600);
+    setResizable(false);
+    setLayout(null);
+    setVisible(true);
     }
 }
