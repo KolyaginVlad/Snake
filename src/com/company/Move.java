5e20a86e8c4d4699b35a85a9c9d0e54f461325f5
SofiaMarchinskaya <52950694+SofiaMarchinskaya@users.noreply.github.com>
2019-07-16 15:56:21 +0300

    Update Move.java

--- src/com/company/Move.java
+++ src/com/company/Move.java
@@ -11,20 +11,20 @@ public class Move extends KeyAdapter{
 public void keyPressed(KeyEvent event){
 if(event.getKeyCode()==KeyEvent.VK_DOWN&&HeadSneake.direction!=HeadSneake.UP){
 HeadSneake.direction=HeadSneake.DOWN;
-HeadSneake.y+=step;
+
 }
 if(event.getKeyCode()==KeyEvent.VK_UP&&HeadSneake.direction!=HeadSneake.DOWN){
 HeadSneake.direction = HeadSneake.UP;
-    HeadSneake.y-=step;
+
 }
 
 if(event.getKeyCode()==KeyEvent.VK_LEFT&&HeadSneake.direction!=HeadSneake.RRIGHT){
 HeadSneake.direction = HeadSneake.LEFT;
-    HeadSneake.x-=step;
+
 }
 if(event.getKeyCode()==KeyEvent.VK_RIGHT&&HeadSneake.direction!=HeadSneake.LEFT){
 HeadSneake.direction = HeadSneake.RRIGHT;
-    HeadSneake.x+=step;
+
 }
 if(event.getKeyCode()==KeyEvent.VK_SPACE){
 
