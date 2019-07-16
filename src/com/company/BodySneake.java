6a4731a45084171b43dddb63c959ddb22383f959
pro100fantom <50174448+pro100fantom@users.noreply.github.com>
2019-07-16 17:31:33 +0300

    Update BodySneake.java

--- src/com/company/BodySneake.java
+++ src/com/company/BodySneake.java
@@ -9,9 +9,9 @@ public class BodySneake {
     //Тел будет много, поэтому не статик
     //Все файлы java  в одном пакете поэтому модификатор доступа не нужен
 
-    static Image image;
-    static int x;
-    static int y;
+     Image image;
+     int x;
+     int y;
 
     public BodySneake(int x,int y){
         this.x = x;
