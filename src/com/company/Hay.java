0336a787cec43bbb56b89d45a99a6b86ba07bf5f
pro100fantom <50174448+pro100fantom@users.noreply.github.com>
2019-07-16 14:46:54 +0300

    hay update

--- src/com/company/Hay.java
+++ src/com/company/Hay.java
@@ -5,12 +5,17 @@ import java.awt.*;
 public class Hay {
     //Сена может быть много, поэтому не статик
     //Все файлы java  в одном пакете поэтому модификатор доступа не нужен
-    Image image;
-    int x;
-    int y;
+   static Image image;
+   static int x;
+   static int y;
     public Hay(){
         this.x=x;
         this.y=y;
         //Присваиваем изображению файл с изображением
+    }
+    public static void eatHay(){
+        int newX = (int)(Math.random()*Window.WIDTH);
+        int newY =(int)(Math.random()*Window.HEIGHT);
+        if (newX)
     }
 }
